9534ceae288e1c98f6007f160c796ee7886c2eed
Marian Huliak <marian.guliak@gmail.com>
2024-08-07 16:30:14 +0300

    done

--- app/pages/BurgerSlider/BurgerSlider.js
+++ app/pages/BurgerSlider/BurgerSlider.js
@@ -18,7 +18,7 @@ import { motion } from "framer-motion";
 
 const burgerVariants = {
   hidden: { opacity: 0,y:0,transition: { type: "spring", duration: 2 } },
-  visible: { opacity: 1,y: 40, transition: {  duration: 1 } },
+  visible: { opacity: 1,y: 20, transition: {  duration: 1 } },
   exit: { opacity: 0, y: 40, transition: { duration: 1 }}
 };
 
